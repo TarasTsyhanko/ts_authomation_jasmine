0c5d3f7ceeabdfb9fc0fa243b2b6d3d7648a9e5d
@taras_tsyhanko <Taras_Tsyhanko@epam.com>
2021-12-16 17:31:08 +0200

    TAF_TS: update client with allure

--- lib/http-client.ts
+++ lib/http-client.ts
@@ -1,50 +1,53 @@
 import fetch from 'node-fetch';
+import {Reporter} from "./reporter";
+import {ContentType} from "allure2-js-commons";
 
 export default class HttpClient {
 
-    public async get(url: string, config?: any, headers?: any, params = {},) {
-        const query = new URLSearchParams(params)
-        return HttpClient.executeRequest(url + query, 'GET', headers)
+    public async get(url: string, headers: any, params = {}) {
+        return await HttpClient.executeRequest(url + `?${new URLSearchParams(params)}`, 'GET', headers)
     }
 
-    public async post(url: string, body?: any, headers?: any) {
-        return HttpClient.executeRequest(url, 'POST', headers, body)
+    public async post(url: string, body: any, headers?: any) {
+        return await HttpClient.executeRequest(url, 'POST', headers, body)
     }
 
     public async patch(url: string, body?: any, headers?: any) {
-        return HttpClient.executeRequest(url, 'PATCH', headers, body)
+        return await HttpClient.executeRequest(url, 'PATCH', headers, body)
     }
 
     public async delete(url: string, headers?: any) {
-        return HttpClient.executeRequest(url, 'DELETE', headers)
+        return await HttpClient.executeRequest(url, 'DELETE', headers)
     }
 
-    private static async executeRequest(url: string, method: string, headers?: any, body?: any) {
-        logRequest(method, url, body, headers)
+    private static async executeRequest(url: string, method: string, headers: any, body?: any) {
+        await logRequest(method, url, body, headers);
         const response = await fetch(url, {method: method, body: body, headers: headers});
+        const response_v2 = response.clone();
         await logResponse(response)
-        return response
+        return response_v2
     }
+
 }
 
-function logRequest(method: string, url: string, body: any, headers: any): void {
-    const logRequest = {
+async function logRequest(method: string, url: string, body: any, headers: any): Promise<void> {
+    const requestData = {
         url: url,
         method: method,
         headers: headers,
         body: parser(body)
     };
-    console.log(logRequest)
+    await Reporter.attachFile('Request', `Request : ${JSON.stringify(requestData, null, 2)}`, ContentType.JSON)
 }
 
 async function logResponse(response: any): Promise<void> {
-    const logResponse = {
+    const responseData = {
         transactionTime: `$ seconds`,
         statusCode: response.status,
         headers: await response.headers,
         body: parser(await response.text())
     };
-    console.log(logResponse)
+    await Reporter.attachFile('Response', `Response : ${JSON.stringify(responseData, null, 2)}`, ContentType.JSON)
 }
 
 const parser = function (json: string) {
